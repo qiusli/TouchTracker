0dcb450216387ad83baeba08e5d344534ed978e0
Qiushi Li <qius.li3@gmail.com>
2015-06-03 16:25:41 -0500

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Qiushi Li on 6/3/15.
+//  Copyright (c) 2015 BigNerdRanch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchTracker
+//
+//  Created by Qiushi Li on 6/3/15.
+//  Copyright (c) 2015 BigNerdRanch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
